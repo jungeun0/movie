659cc34a3bfd88b5e7a105901c7737990c7795c2
jungeun0 <sky_fall7@naver.com>
2018-12-06 15:02:42 +0900

    .
    .

---
+++ main.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "movie.h"
+#include "linkedList.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	FILE *fp; //FILE pointer for reading movie data 
+	char name[200]; //movie name
+	char country[10]; //movie country
+	int runTime; //movie runtime
+	float score; //movie score
+	
+	int exit_flag = 0; //flag variable for while loop
+	int option; //user input option
+	void *list, *mvInfo; //pointers for linked list and a specific structure instance for a movie data
+	/* linked list를 가리키는, list를 가리키는 포인터 변수 *list, 노드를 가리키는 포인터 필요함 이게 node 포인터*/ 
+	int (*repFunc)(void* obj, void* arg); //function pointer for using list_repeatFunc() function 함수포인터
+	void *arg; //a void pointer for passing argument to repFunc
+	int cnt; //integer variable
+	
+	 
+	//1. reading the movie.dat-----------------------------
+	
+	
+    
+	//1.1 FILE open:
+		fp = fopen("movie.dat","r");
+	if(fp != NULL){
+		printf("파일을 열었습니다.\n");
+	} 
+	else{
+		printf("파일 오픈 실패.\n");
+		return 0;
+	}
+	
+	//1.2 list generation (use function list_genList() )
+	list = list_genList();
+	
+	//1.3 read each movie data from the file and add it to the linked list
+	while (  )/* read name, country, runtime and score*/
+	{	//읽어온 파일을 열어서 함수를 이용해서 구조체에 저장 
+		
+		
+		
+		
+		//generate a movie info instance(mvInfo) with function mv_genMvInfo()
+	  list_addTail(mvInfo, list);
+	}
+   
+    fclose(fp);
+    
+	//1.4 FILE close
+	
+	//2. program start
+	while(exit_flag == 0)
+	{
+		//2.1 print menu message and get input option
+		
+		
+		printf("-------------------- menu --------------------\n");
+		printf("1. print all the movies\n");
+		printf("2. search for specific country movies\n");
+		printf("3. search for specific runtime movies\n");
+		printf("4. search for specific score movies\n");
+		printf("-------------------- menu --------------------\n");
+		
+		
+		switch(option)
+		{
+			case 1: //print all the movies
+				printf("\nprinting all the movies in the list.....\n\n\n");
+				printf("----------------------------------------\n");
+				
+				repFunc = mv_printAll;
+				arg = NULL;
+				break;
+				
+			case 2: //print movies of specific country
+
+				break;
+				
+			case 3: //print movies with long runtime
+
+				break;
+				
+			case 4: //print movies with high score
+				
+				break;
+				
+			case 5:
+				printf("\n\nBye!\n\n");
+				exit_flag = 1;
+				
+				break;
+				
+			default:
+				printf("wrong command! input again\n");
+				break;
+		}
+		
+		//2.2 printing operation by function pointer (list_repeatFunc() is called here)
+		//2.3 print number of movies
+	}
+	
+	return 0;
+} 
